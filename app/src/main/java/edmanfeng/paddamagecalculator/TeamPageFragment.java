b85db344ba1060f1e63e8c30fa3c292de0684ec5
Edman Feng <edmanfeng@hotmail.com>
2017-04-03 08:10:14 -0700

    TeamPage: switched pickers to spinners

--- app/src/main/java/edmanfeng/paddamagecalculator/TeamPageFragment.java
+++ app/src/main/java/edmanfeng/paddamagecalculator/TeamPageFragment.java
@@ -10,18 +10,21 @@ import android.widget.ArrayAdapter;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by t7500 on 4/3/2017.
  */
 
 public class TeamPageFragment extends Fragment {
 
-    private final int MIN_ORBS = 0;
+    private final int MIN_ORBS = 1;
     private final int MAX_ORBS = 42;
 
     private Spinner mComboTypeSpinner;
-    private NumberPicker mComboOrbNumberPicker;
-    private NumberPicker mComboEnhanceNumberPicker;
+    private Spinner mComboOrbNumberSpinner;
+    private Spinner mComboEnhanceNumberSpinner;
     private Spinner mComboShapeSpinner;
 
 
@@ -37,46 +40,48 @@ public class TeamPageFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater
                 .inflate(R.layout.fragment_team_page,container, false);
-        /*
+
         mComboTypeSpinner = (Spinner)view.findViewById(R.id.combo_type_spinner);
         ArrayAdapter<CharSequence> comboTypeAdapter = ArrayAdapter.createFromResource(
                 getActivity(), R.array.orb_types, android.R.layout.simple_spinner_item);
         comboTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mComboTypeSpinner.setAdapter(comboTypeAdapter);
-*/
-        /*
-        mComboOrbNumberPicker = (NumberPicker)view
-                .findViewById(R.id.combo_orb_number_picker);
-        mComboOrbNumberPicker.setMinValue(MIN_ORBS);
-        mComboOrbNumberPicker.setMaxValue(MAX_ORBS);
-        mComboOrbNumberPicker.setWrapSelectorWheel(true);
-        mComboOrbNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                // set the max enhanced orbs in a match to be the same
-                // as total orbs in the match
-                mComboEnhanceNumberPicker.setMaxValue(newVal);
-            }
-        });
 
-        mComboEnhanceNumberPicker = (NumberPicker)view
-                .findViewById(R.id.combo_enhance_number_picker);
-        mComboEnhanceNumberPicker.setMinValue(MIN_ORBS);
-        mComboEnhanceNumberPicker.setMaxValue(MAX_ORBS);
-        mComboEnhanceNumberPicker.setWrapSelectorWheel(true);
-        mComboEnhanceNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        List<Integer> orbCountLimits = new ArrayList<>();
+        for (int i = MIN_ORBS; i < MAX_ORBS; i++) {
+            orbCountLimits.add(i);
+        }
+        mComboOrbNumberSpinner = (Spinner)view
+                .findViewById(R.id.combo_orb_number_spinner);
+        ArrayAdapter<Integer> comboOrbNumAdapter = new ArrayAdapter<Integer>(
+                getActivity(), android.R.layout.simple_spinner_item, orbCountLimits);
+        comboOrbNumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mComboOrbNumberSpinner.setAdapter(comboOrbNumAdapter);
+
+
+        mComboEnhanceNumberSpinner = (Spinner) view
+                .findViewById(R.id.combo_enhance_number_spinner);
+        ArrayAdapter<Integer> comboEnhanceNumAdapter = new ArrayAdapter<Integer>(
+                getActivity(), android.R.layout.simple_spinner_item, orbCountLimits);
+        comboEnhanceNumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mComboEnhanceNumberSpinner.setAdapter(comboEnhanceNumAdapter);
+        /*
+        mComboEnhanceNumberSpinner.setMinValue(MIN_ORBS);
+        mComboEnhanceNumberSpinner.setMaxValue(MAX_ORBS);
+        mComboEnhanceNumberSpinner.setWrapSelectorWheel(true);
+        mComboEnhanceNumberSpinner.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 // placeholder
 
             }
-        });
+        });*/
 
         mComboShapeSpinner = (Spinner)view.findViewById(R.id.combo_shape_spinner);
         ArrayAdapter<CharSequence> comboShapeAdapter = ArrayAdapter.createFromResource(
                 getActivity(), R.array.combo_shape_types, android.R.layout.simple_spinner_item);
         comboShapeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mComboShapeSpinner.setAdapter(comboShapeAdapter);*/
+        mComboShapeSpinner.setAdapter(comboShapeAdapter);
         return view;
     }
 }
