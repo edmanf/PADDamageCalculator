13a4b5b86afb5fcbc5be559b3131c608ec25dc69
Edman Feng <edmanf@cs.washington.edu>
2017-04-08 16:30:51 -0700

    Setup the monster database

---
+++ app/src/main/java/edmanfeng/paddamagecalculator/MonsterLab.java
@@ -0,0 +1,94 @@
+package edmanfeng.paddamagecalculator;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.UUID;
+
+import edmanfeng.paddamagecalculator.GameModel.Monster;
+import edmanfeng.paddamagecalculator.database.MonsterBaseHelper;
+import edmanfeng.paddamagecalculator.database.MonsterCursorWrapper;
+import edmanfeng.paddamagecalculator.database.PadDbSchema;
+import edmanfeng.paddamagecalculator.database.PadDbSchema.MonsterTable;
+
+/**
+ * Created by t7500 on 4/7/2017.
+ */
+
+public class MonsterLab {
+    private static MonsterLab sMonsterLab;
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    private MonsterLab(Context context) {
+        mContext = context;
+        mDatabase = new MonsterBaseHelper(context)
+                .getWritableDatabase();
+    }
+
+    public MonsterLab get(Context context) {
+        if (sMonsterLab == null) {
+            sMonsterLab = new MonsterLab(context);
+        }
+        return sMonsterLab;
+    }
+
+    public void addMonster(Monster monster) {
+        ContentValues contentValues = getContentValues(monster);
+        mDatabase.insert(MonsterTable.NAME, null, contentValues);
+    }
+
+    public void updateMonster(Monster monster) {
+
+    }
+
+    public void deleteMonster(Monster monster) {
+
+    }
+
+    public Monster getMonster(UUID uuid) {
+        String query = MonsterTable.Cols.UUID + " = ?";
+        String[] args = new String[] { uuid.toString() };
+
+        MonsterCursorWrapper cursor = queryMonsters(query, args);
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+
+            cursor.moveToFirst();
+            return cursor.getMonster();
+        } finally {
+            cursor.close();
+        }
+    }
+
+    public ContentValues getContentValues(Monster monster) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MonsterTable.Cols.UUID, monster.getId().toString());
+        contentValues.put(MonsterTable.Cols.NAME, monster.getName());
+        contentValues.put(MonsterTable.Cols.NUM, monster.getNum());
+        contentValues.put(MonsterTable.Cols.BASE_HP, monster.getHp());
+        contentValues.put(MonsterTable.Cols.BASE_ATK, monster.getAtk());
+        contentValues.put(MonsterTable.Cols.BASE_RCV, monster.getRcv());
+
+        return contentValues;
+    }
+
+    public MonsterCursorWrapper queryMonsters(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                MonsterTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new MonsterCursorWrapper(cursor);
+    }
+
+}
--- app/src/main/java/edmanfeng/paddamagecalculator/MonsterSource.java
+++
@@ -1,9 +0,0 @@
-package edmanfeng.paddamagecalculator;
-
-/**
- * Created by t7500 on 4/7/2017.
- */
-
-public class MonsterSource {
-
-}
